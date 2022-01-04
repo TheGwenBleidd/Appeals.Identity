6518bb9081c227ec8c73fce539e5f792677b38e8
meirlan <marabpai@teq.kz>
2022-01-04 18:19:28 +0600

    small fixes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 ﻿using Appeals.Identity.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Appeals.Identity.Controllers
@@ -6,6 +8,19 @@ namespace Appeals.Identity.Controllers
     [Route("[controller]/[action]")]
     public class AuthController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IIdentityServerInteractionService _identityServerInteractionService;
+
+        public AuthController(SignInManager<AppUser> signInManager, 
+            UserManager<AppUser> userManager, 
+            IIdentityServerInteractionService identityServerInteractionService)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _identityServerInteractionService = identityServerInteractionService;
+        }
+
         [HttpGet]
         public IActionResult Login(string returnUrl)
         {
@@ -17,9 +32,68 @@ namespace Appeals.Identity.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model) 
+        public async Task<IActionResult> Login(LoginViewModel model) 
         {
+            if (!ModelState.IsValid) 
+                return View(model);
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "User not found");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
+            if (result.Succeeded) 
+            {
+                if (!string.IsNullOrWhiteSpace(model.ReturnUrl))
+                    return Redirect(model.ReturnUrl);
+                else
+                    return BadRequest("Return url cannot be null");
+            }
+            
+            ModelState.AddModelError(string.Empty, "Login error");
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl) 
+        {
+            var model = new RegisterViewModel()
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model) 
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new AppUser(model);
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded) 
+            {
+                await _signInManager.SignInAsync(user, false);
+                if (!string.IsNullOrWhiteSpace(model.ReturnUrl))
+                    return Redirect(model.ReturnUrl);
+                else
+                    return BadRequest(model.ReturnUrl);
+            }
+            ModelState.AddModelError(string.Empty, "Registration error");
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId) 
+        { 
+            await _signInManager.SignOutAsync();
+            var logoutRequest = await _identityServerInteractionService.GetLogoutContextAsync(logoutId);
+            return Redirect(logoutRequest.PostLogoutRedirectUri);
         }
     }
 }
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -4,6 +4,12 @@ namespace Appeals.Identity.Models
 {
     public class AppUser : IdentityUser
     {
+        public AppUser(RegisterViewModel model)
+        {
+            this.UserName = model.UserName;
+            this.Email = model.UserName;
+        }
+
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? MiddleName { get; set; }
